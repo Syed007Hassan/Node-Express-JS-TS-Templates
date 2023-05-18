6742a209037c1bc324bd612597eb6a2766e3b162
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-05-19 01:33:46 +0500

    packages imported

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello World!");
+import express from "express";
+import http from "http";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import bodyParser from "body-parser";
+import path from "path";
+
+dotenv.config();
+
+const app = express();
+const dir = __dirname;
+const port: string = process.env.PORT;
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(express.json({ limit: "50mb" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
