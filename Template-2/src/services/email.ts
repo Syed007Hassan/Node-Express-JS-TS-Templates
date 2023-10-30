5399315853c2cd823b7c6fc6439febb06e4b6149
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-10-31 01:36:34 +0500

    template 2 refactored

--- Template-2/src/services/email.ts
+++ Template-2/src/services/email.ts
@@ -1,23 +1,23 @@
-import sgMail from "@sendgrid/mail";
+// import sgMail from "@sendgrid/mail";
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
-export const sendVerificationEmail = async (to: string, token: string) => {
+// export const sendVerificationEmail = async (to: string, token: string) => {
 
-	const msg = {
-		to: to,
-		from: process.env.SENDGRID_SENDER_EMAIL as string,
-		subject: "Verify your email address",
-		text: `Please verify your email address by clicking the link below:
-		 	${process.env.CLIENT_URL}/login/verify/${token}`,
-		html: `<p>Please verify your email address by clicking the link below:</p>
-		<a href="${process.env.CLIENT_URL}/login/verify/${token}">Verify Email</a>`,
-	};
+// 	const msg = {
+// 		to: to,
+// 		from: process.env.SENDGRID_SENDER_EMAIL as string,
+// 		subject: "Verify your email address",
+// 		text: `Please verify your email address by clicking the link below:
+// 		 	${process.env.CLIENT_URL}/login/verify/${token}`,
+// 		html: `<p>Please verify your email address by clicking the link below:</p>
+// 		<a href="${process.env.CLIENT_URL}/login/verify/${token}">Verify Email</a>`,
+// 	};
 
-	try {
-		await sgMail.send(msg);
-	} catch (err) {
-		console.log(err);
-	}
+// 	try {
+// 		await sgMail.send(msg);
+// 	} catch (err) {
+// 		console.log(err);
+// 	}
 
-};
+// };
